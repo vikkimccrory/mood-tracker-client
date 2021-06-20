fa2bf7ab7bf56c7abcfbb5ba2408240e674d7c2a
vikkimccrory <victoriajmccrory@gmail.com>
2021-06-19 20:04:33 -0400

    Cleared out console logs

--- assets/scripts/entries/events.js
+++ assets/scripts/entries/events.js
@@ -9,7 +9,6 @@ const api = require('./api')
 const onCreateEntry = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
-  console.log(data)
 
   api.createEntry(data)
     .then(ui.createEntrySuccess)
